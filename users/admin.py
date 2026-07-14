35a5860c8d5642310edbaeb3dbc4deca6396a92d
IYYAS <iyyasiyyas35@gmail.com>
2026-07-14 16:35:58 +0530

    dhd

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models 
+from .models import User, Vendor, SubscriptionPlan
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Vendor & role', {'fields': ('vendor', 'role')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'phone', 'role', 'vendor', 'is_active', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'role')
+    search_fields = ('username', 'email', 'phone')
+    ordering = ('username',)
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'plan', 'subscription_ends_at', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('plan', 'created_at', 'is_active')
+
+
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'limit_messages_monthly', 'limit_contacts', 'limit_agents', 'is_active')
+    search_fields = ('name',)
+    list_filter = ('is_active',)
 
 
